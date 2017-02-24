27d5408f0dfe78de54c96e96e12bed7b71e92d93
Riccardo De Maria <riccardodemaria@gmail.com>
2017-02-24 06:15:47 +0100

    create package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='pyoptics',
+        version='0.0.0',
+        description='Particle Beam Optics Tools',
+        author='Riccardo De Maria',
+        author_email='riccardo.de.maria@cern.ch',
+        url='https://github.com/rdemaria/pyoptics',
+        packages=find_packages(),
+        install_requires=['numpy','matplotlib'],
+)
+
