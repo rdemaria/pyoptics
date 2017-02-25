66d23107659c80da9d8d6109b392435852df6891
Riccardo De Maria <riccardodemaria@gmail.com>
2017-02-25 10:42:26 +0100

    fixing relativistic factor in madbeam

--- pyoptics/madbeam.py
+++ pyoptics/madbeam.py
@@ -11,6 +11,8 @@ def pt2rpp(pt,beta0=1):
     """
     return 1/np.sqrt(pt**2+2*pt/beta0+1)
 
+def pt2opd(pt,beta0=1):
+    return np.sqrt(pt**2+2*pt/beta0+1)
 
 class MadBeam(object):
     @classmethod
@@ -40,19 +42,21 @@ class MadBeam(object):
        out['elemid'] =0
        out['turn']   =self.turn
        out['state']  =0
-       out['p0c']    =self.twiss.param['pc']
-       out['e0']     =self.twiss.param['energy']
-       out['m0']     =self.twiss.param['mass']
+       out['p0c']    =self.twiss.param['pc']*1e6
+       out['e0']     =self.twiss.param['energy']*1e6
+       out['m0']     =self.twiss.param['mass']*1e6
        out['gamma0'] =self.twiss.param['gamma']
-       out['beta0']  =out['p0c']/out['e0']
-       out['sigma']  =out['tau']*out['beta0']
-       out['psigma'] =out['ptau']/out['beta0']
-       out['rpp']    =pt2rpp(self.pt,out['beta0'])
-       out['rvv']    =1/(out['rpp']*(1+self.pt*out['beta0']**2))
-       out['delta']  =1/out['rpp']-1
-       out['beta']   =self.pt*out['p0c']-out['p0c']
-       out['gamma']  =self.e/out['m0']
-       out['energy'] =self.e
+       beta0         =out['p0c']/out['e0']
+       out['beta0']  =beta0
+       out['sigma']  =out['tau']*beta0
+       out['psigma'] =out['ptau']/beta0
+       opd           =pt2opd(self.pt,beta0)
+       out['delta']  =opd-1
+       out['rpp']    =1/opd
+       out['beta']   =opd/(1/beta0+self.pt)
+       out['rvv']    =beta0/out['beta']
+       out['energy'] =self.e*1e6
+       out['gamma']  =1/np.sqrt(1-out['beta']**2)
        out['q0']     =self.twiss.param['charge']
        out['q']      =one
        out['chi']    =one
